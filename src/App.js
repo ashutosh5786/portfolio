b488b0bd3300c16bc31ada7c028843c2681bbcd8
ashutosh5786 <ashutosh5786@outlook.com>
2021-03-03 23:21:52 +0530

    started to work on the Intro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // Importing the AppBar 
 import MyAppBar from './components/navigation'
-
+// Importing the Introduction
+// import 
 
 
 
 // Importing the Styles
 import "./style/navigation.css"
+import "./style/intro.css"
 
 function App() {
   return (
